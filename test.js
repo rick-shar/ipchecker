23e4726cf7fa6134ad5e632d6da5e2db9b908743
rick <rick@joinhorizons.com>
2017-11-03 15:31:49 -1000

    [added] tests for checking main logic

--- test.js
+++ test.js
@@ -1,10 +1,15 @@
-var test = require('unit.js');
-var str = 'Hello, world!';
+const assert = require('assert');
+const LEVEL2_TABLE = require('./data/level2.json');
+const checkRanges = require('./server.js').checkRanges;
 
-test
-    .string(str)
-    .startsWith('Hello');
+describe("checkRanges on local firehol_level2", (done) => {
+    it("It should reject ip 5.9.25.66", () => {
+        assert.equal(checkRanges("5.9.25.66", LEVEL2_TABLE), '5.9.25.66/31');
+        // TODO: remove these checks to their own it statements
+        assert.equal(checkRanges("5.9.25.67", LEVEL2_TABLE), '5.9.25.66/31');
+        assert.equal(checkRanges("5.9.25.68", LEVEL2_TABLE), '5.9.25.68/30');
+        assert.equal(checkRanges("5.9.25.69", LEVEL2_TABLE), '5.9.25.68/30');
+        assert.equal(checkRanges("5.9.250.79", LEVEL2_TABLE), false);
+    });
+});
 
-if (test.string(str).startsWith('Hello')) {
-    console.log('Passed');
-}
