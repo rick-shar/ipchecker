486089a37465d2891576fe0a060c667cef05c4be
rick <rick@joinhorizons.com>
2017-11-06 22:09:40 -1000

    [linted] :)

--- util/cidr.js
+++ util/cidr.js
@@ -1,39 +1,40 @@
 // TODO: add a descriptive heading to this page
 const IPv4 = require('ip-address').Address4;
+
 function ipStringToZeroPad(ipString, prefixLength) {
-    // TODO: code duplication move to utils once repos are merged
-    return new IPv4(ipString)
-        .binaryZeroPad()
-        .slice(0, prefixLength);
+  // TODO: code duplication move to utils once repos are merged
+  return new IPv4(ipString)
+    .binaryZeroPad()
+    .slice(0, prefixLength);
 }
 
 function formPrefixArray(ipString) {
-    // TODO: extend to be configurable and remove magic number
-    const binaryString = ipStringToZeroPad(ipString);
-    let temp = Array(32)
-        .fill()
-        .map((x, i) => i + 1)
-        .map(length => binaryString.slice(0, length));
-    return temp;
+  // TODO: extend to be configurable and remove magic number
+  const binaryString = ipStringToZeroPad(ipString);
+  const temp = Array(32)
+    .fill()
+    .map((x, i) => i + 1)
+    .map(length => binaryString.slice(0, length));
+  return temp;
 }
 
 function checkRanges(address, table) {
-    const addressPrefixes = formPrefixArray(address);
-    let found = false;
+  const addressPrefixes = formPrefixArray(address);
+  let found = false;
 
-    // TODO: mourn having to use a for loop (RIP)
-    for (let i = 0; i < addressPrefixes.length; i++) {
-        let prefix = addressPrefixes[i];
-        if (table["ranges"][prefix]) {
-            found = table["ranges"][prefix];
-            break;
-        }
+  // TODO: mourn having to use a for loop (RIP)
+  for (let i = 0; i < addressPrefixes.length; i += 1) {
+    const prefix = addressPrefixes[i];
+    if (table.ranges[prefix]) {
+      found = table.ranges[prefix];
+      break;
     }
-    return found;
+  }
+  return found;
 }
 
 module.exports = {
-    // exports for unit testing
-    checkRanges
+  // exports for unit testing
+  checkRanges,
 
-}
+};
--- util/ipcheck.js
+++ util/ipcheck.js
@@ -2,78 +2,78 @@
 // TODO: add some notes here on what this file is for
 const PREFIX_TABLES = Array(4).fill(null);
 // TODO: read filename array from env
-const FILENAME_KEY_ARRAY = ["firehol_level1|latest", "firehol_level2|latest", "firehol_level3|latest", "firehol_level4|latest"];
+const FILENAME_KEY_ARRAY = ['firehol_level1|latest', 'firehol_level2|latest', 'firehol_level3|latest', 'firehol_level4|latest'];
 
 // TODO: force sync write to prefix table for initial setup
-const checkRanges = require('./cidr').checkRanges;
-const AWS = require('aws-sdk'); 
+const { checkRanges } = require('./cidr');
+const AWS = require('aws-sdk');
+
 const s3 = new AWS.S3();
 // TODO: read const for update frequency from env
 const UPDATE_FREQUENCY = 60 * 1000 * 5;
 
 function processUpdates(jsonArray) {
-    // TODO: make this more cautious of failures
-    Array(4)
-        .fill(null)
-        .forEach((x, i) => PREFIX_TABLES[i] = JSON.parse(jsonArray[i].Body));
+  // TODO: make this more cautious of failures
+  Array(4)
+    .fill(null)
+    .forEach((x, i) => {
+      PREFIX_TABLES[i] = JSON.parse(jsonArray[i].Body);
+    });
 }
 
 function update() {
-    // TODO: figure out tradeoffs for updating all files vs one level at a time
-    // TODO: remove magic strings
-    // TODO: handle rejections
-    const promises = FILENAME_KEY_ARRAY.map(key => s3.getObject({Bucket: process.env.BUCKET, Key: key}).promise());
-    return Promise
-        .all(promises)
-        .then(processUpdates);
+  // TODO: figure out tradeoffs for updating all files vs one level at a time
+  // TODO: remove magic strings
+  // TODO: handle rejections
+  const promises = FILENAME_KEY_ARRAY
+    .map(key => s3.getObject({ Bucket: process.env.BUCKET, Key: key }).promise());
+  return Promise
+    .all(promises)
+    .then(processUpdates);
 }
 
 function checkUpdate() {
-    // TODO: move config out to environment variables
-    // TODO: handle failures and remove strange method to check last updated time
-    // TODO: remove magic number for timecheck
-    const now = new Date().getTime();
-    const lastUpdateTime = new Date(PREFIX_TABLES[0].timestamp).getTime();
-    if (now - lastUpdateTime > UPDATE_FREQUENCY) {
-        update(now);
-    }
-
+  // TODO: move config out to environment variables
+  // TODO: handle failures and remove strange method to check last updated time
+  // TODO: remove magic number for timecheck
+  const now = new Date().getTime();
+  const lastUpdateTime = new Date(PREFIX_TABLES[0].timestamp).getTime();
+  if (now - lastUpdateTime > UPDATE_FREQUENCY) {
+    update(now);
+  }
 }
 
 function rejectIP(reason) {
-    checkUpdate();
-    return {
-        valid: false,
-        // TODO: have "reason" be a url containing more information for the user
-        reason
-    };
+  checkUpdate();
+  return {
+    valid: false,
+    // TODO: have "reason" be a url containing more information for the user
+    reason,
+  };
 }
 
 function acceptIP() {
-    checkUpdate();
-    return {
-        valid: true,
-        // TODO: have reason be a url containing more information for the user
-        reason: null
-    };
+  checkUpdate();
+  return {
+    valid: true,
+    // TODO: have reason be a url containing more information for the user
+    reason: null,
+  };
 }
 
-function checkIP(request) {
-    // TODO: Enforce types and ranges here!
-    // TODO: Will need to check token field if not AWS
-    // TODO: levels are not nested! need to apply n-1 table checks for level n
-    const table = PREFIX_TABLES[request.level - 1];
-    const address = request.address;
-    const reasonForReject = table["ips"][address] || checkRanges(address, table);
-    if(reasonForReject) {
-        return rejectIP(reasonForReject);
-    } else {
-        return acceptIP();
-    }
-
+function checkIP({ address, level }) {
+  // TODO: Enforce types and ranges here!
+  // TODO: Will need to check token field if not AWS
+  // TODO: levels are not nested! need to apply n-1 table checks for level n
+  const table = PREFIX_TABLES[level - 1];
+  const reasonForReject = table.ips[address] || checkRanges(address, table);
+  if (reasonForReject) {
+    return rejectIP(reasonForReject);
+  }
+  return acceptIP();
 }
 
 module.exports = {
-    checkIP,
-    update
-}
+  checkIP,
+  update,
+};
